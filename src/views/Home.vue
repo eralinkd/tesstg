b8007fd479ec35cf511e5d2fa76e23c020b57151
eralinkd <eralinkd@gmail.com>
2024-12-20 21:04:08 +0200

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+
+    
+</template>
+
+<script>
+    export default {
+        name: 'Home',
+        data() {
+            return {
+                htmlResponse: '',
+            }
+        },
+
+
+        async mounted() {
+            fetch('https://uk.tgstat.com/').then(data => {
+                this.htmlResponse = data
+                console.log(this.htmlResponse)
+            })
+        }
+    }
+</script>
